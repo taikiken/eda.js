53c452648bf136ba67ab17cc84ff7fd339f3b3cb
taikiken <denkosekka@gmail.com>
2015-09-01 20:19:16 +0900

    API Docs

---
+++ api.js
@@ -0,0 +1,32 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Kana",
+        "List",
+        "Num",
+        "Query"
+    ],
+    "modules": [
+        "Eda",
+        "Kana",
+        "Num"
+    ],
+    "allModules": [
+        {
+            "displayName": "Eda",
+            "name": "Eda"
+        },
+        {
+            "displayName": "Kana",
+            "name": "Kana",
+            "description": "日本語判定処理"
+        },
+        {
+            "displayName": "Num",
+            "name": "Num",
+            "description": "数にまつわる Utilities"
+        }
+    ],
+    "elements": []
+} };
+});
